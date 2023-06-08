01b6671b898616f82e5cb5f19099719f84249e37
Betul Topkan <betultopkann@gmail.com>
2023-06-09 00:40:53 +0300

    add controller

---
+++ AspNetMvcBlog/App.Web.Mvc/Controllers/Admin/BlogController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Mvc.Controllers.Admin
+{
+    public class BlogController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
